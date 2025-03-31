8084f94aab2fb9f9d49c080bd206d4d54267b044
PeterYuan <1215729026@qq.com>
2025-03-31 14:52:28 +0800

    init repo

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import HeroSection from './components/HeroSection'
+import ProductGallery from './components/ProductGallery'
+import FeatureHighlights from './components/FeatureHighlights'
+import SpecificationsPanel from './components/SpecificationsPanel'
+import SocialProof from './components/SocialProof'
+import PricingModule from './components/PricingModule'
+import './App.css'
+
+function App() {
+  const [activeTab, setActiveTab] = useState('specs')
+
+  return (
+    <div className="app">
+      <HeroSection />
+      <ProductGallery />
+      <FeatureHighlights />
+      <SpecificationsPanel activeTab={activeTab} setActiveTab={setActiveTab} />
+      <SocialProof />
+      <PricingModule />
+    </div>
+  )
+}
+
+export default App
